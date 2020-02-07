bc0ea0c2f2d95d62d2792604850de1787ed5cb8b
Uriel Medina <umedina@unitedvirtualities.com>
2020-02-07 09:02:58 -0700

    Index-0702

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const server = express();
 
 
 server.get('/', (req, res) => {
-	res.send("Hello World");
+	res.send("Hello World!");
 }).listen(3000, () => {
 	console.log("Server is running...");
 });
