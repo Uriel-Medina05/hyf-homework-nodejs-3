fe16dc2fa1aa4de85a4aaed4631dd0a155d77aed
Uriel Medina <umedina@unitedvirtualities.com>
2020-02-07 11:28:14 -0700

    AddUser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const express = require ('express');
 const server = express();
 
+let users = [];
 
 server.get('/', (req, res) => {
 	res.send("Hello World!");
-}).listen(3000, () => {
+});
+
+server.get('/users', (req, res) => {
+	res.json(users);
+});
+
+server.get('/user/:id', (req, res) => {
+	const user = users.find(usr => usr.id == req.params.id);
+	res.json(user);
+});
+
+server.post('/user', (req, res) => {
+	users.push({id: 0});
+	res.json(users);
+});
+
+
+server.listen(3000, () => {
 	console.log("Server is running...");
 });
+
